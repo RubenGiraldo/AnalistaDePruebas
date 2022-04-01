d302014fc1e12d4b6fca23ec918b701e1489a353
RubenGiraldo <39750960+RubenGiraldo@users.noreply.github.com>
2022-03-31 19:24:54 -0500

    Add files via upload

---
+++ ChoucairAcademyPage.java
@@ -0,0 +1,8 @@
+package userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("http://operacion.chuocairtesting.com/academy/login/index.php")
+public class ChoucairAcademyPage extends PageObject{
+}
